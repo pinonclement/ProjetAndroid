c50b7d1f7465ead40e65c5c1e611244b2e4a7a12
pinonclement <clem.pi@live.fr>
2016-12-10 12:35:32 +0100

    Ajout Creation des joueur + popup debut de tour

--- src/com/project/projetandroid/JeuActivity.java
+++ src/com/project/projetandroid/JeuActivity.java
@@ -3,30 +3,110 @@ package com.project.projetandroid;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class JeuActivity extends Activity {
+	
+	//private String user1 = getIntent().getStringExtra("user1");
+	//String user2 = getIntent().getStringExtra("user2");
+	//String user3 = getIntent().getStringExtra("user3");
+	//String user4 = getIntent().getStringExtra("user4");
+	ArrayList al = new ArrayList();
+    private static final int DIALOG_ALERT = 10;
 
 	public void onCreate(Bundle bn) {
 
 		setContentView(R.layout.jeulayout);
 		super.onCreate(bn);
-	/*	for (int i = 0; i < DefinirJoueur().size(); i++) {
-			new Joueur(DefinirJoueur().get(i));
-		} */
+		
+		String user1 = getIntent().getStringExtra("user1");
+		String user2 = getIntent().getStringExtra("user2");
+		String user3 = getIntent().getStringExtra("user3");
+		String user4 = getIntent().getStringExtra("user4");
+		
+		al=DefinirJoueur(user1,user2,user3,user4,al);
+		showDialog(DIALOG_ALERT);
 	}
 
-	public ArrayList<String> DefinirJoueur() {
-	
-		ArrayList<String> listjoueur = null; // Liste de joueurs
-		if (getIntent().getStringExtra("user1").length() > 0)
-			listjoueur.add(getIntent().getStringExtra("user1"));
-		if (getIntent().getStringExtra("user2").length() > 0)
-			listjoueur.add(getIntent().getStringExtra("user2"));
-		if (getIntent().getStringExtra("user3").length() > 0)
-			listjoueur.add(getIntent().getStringExtra("user3"));
-		if (getIntent().getStringExtra("user4").length() > 0)
-			listjoueur.add(getIntent().getStringExtra("user4"));
-		return listjoueur;
+	public ArrayList DefinirJoueur(String a, String b, String c, String d, ArrayList al) {
+
+		if (c.length()==0 && d.length()==0) {
+			Joueur joueur1 = new Joueur(a);
+			Joueur joueur2 = new Joueur(b);
+			al.add(joueur1);
+			al.add(joueur2);
+		}
+		else{
+			if (d.length()==0 ){
+				Joueur joueur1 = new Joueur(a);
+				Joueur joueur2 = new Joueur(b);
+				Joueur joueur3 = new Joueur(c);
+				al.add(joueur1);
+				al.add(joueur2);
+				al.add(joueur3);
+			}
+            else{
+                Joueur joueur1 = new Joueur(a);
+                Joueur joueur2 = new Joueur(b);
+                Joueur joueur3 = new Joueur(c);
+                Joueur joueur4 = new Joueur(d);
+                al.add(joueur1);
+                al.add(joueur2);
+                al.add(joueur3);
+                al.add(joueur4);
+
+            }
+		}
+			
+		return al;
 	}
+
+
+
+
+    /** Called when the activity is first created. */
+
+
+
+    public void onClick(View view) {
+        showDialog(DIALOG_ALERT);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DIALOG_ALERT:
+                // Create out AlterDialog
+                Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(((Joueur) al.get(0)).getPseudo()+"à ton tour !");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Ok", new OkOnClickListener());
+               // builder.setNegativeButton("OK", new CancelOnClickListener());
+                AlertDialog dialog = builder.create();
+                dialog.show();
+        }
+        return super.onCreateDialog(id);
+    }
+
+    private final class CancelOnClickListener implements
+            DialogInterface.OnClickListener {
+        public void onClick(DialogInterface dialog, int which) {
+            Toast.makeText(getApplicationContext(), "Activity will continue",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private final class OkOnClickListener implements
+            DialogInterface.OnClickListener {
+    	 public void onClick(DialogInterface dialog, int which) {
+             Toast.makeText(getApplicationContext(), "C'est parti !!!",
+                     Toast.LENGTH_LONG).show();
+    }
+    }
 }
--- src/com/project/projetandroid/Joueur.java
+++ src/com/project/projetandroid/Joueur.java
@@ -10,6 +10,66 @@ public class Joueur {
 
 	
 
+	public String getPseudo() {
+		return pseudo;
+	}
+
+
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+
+
+	public int getScore_final() {
+		return score_final;
+	}
+
+
+
+	public void setScore_final(int score_final) {
+		this.score_final = score_final;
+	}
+
+
+
+	public int getPate() {
+		return pate;
+	}
+
+
+
+	public void setPate(int pate) {
+		this.pate = pate;
+	}
+
+
+
+	public int getCasserole() {
+		return casserole;
+	}
+
+
+
+	public void setCasserole(int casserole) {
+		this.casserole = casserole;
+	}
+
+
+
+	public int getFourchette() {
+		return fourchette;
+	}
+
+
+
+	public void setFourchette(int fourchette) {
+		this.fourchette = fourchette;
+	}
+
+
+
 	public Joueur (String pseudo){
 		this.pseudo=pseudo;
 		score_final=0;
